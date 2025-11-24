6464e9e528a113c6f1968755b6caa57345f6aa81
talnawande-two <talnawande@gmail.com>
2025-11-24 07:11:21 +0300

    Create Roommate.py file

---
+++ assignment1/Roommate.py
@@ -0,0 +1,17 @@
+class Roommate:
+    def __init__(self, name, age, personality, hobby):
+        self.name = name
+        self.age = age
+        self.personality = personality
+        self.hobby = hobby
+
+    def display_info(self):
+        print("Roommate Details:")
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Personality: {self.personality}")
+        print(f"Hobby: {self.hobby}")
+
+
+r = Roommate("Brian", 20, "Calm and organized", "Watching football")
+r.display_info()
